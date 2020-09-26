da0c72da3c54e7b8c98b145d1b2fd0a7a7c4f5a9
mdsayeed6220 <sayeedofficial6220@gmail.com>
2020-09-26 23:33:15 +0530

    Initialized Project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lets Play a Simple Quiz"),
+        ),
+      ),
+    );
+  }
+}
